f3ce647e695e788ed499d9841a348a9f038c40d6
Guillermo Aser <guilleaser@gmail.com>
2022-06-15 08:35:09 +0200

    c

--- php/configuracion_mails.php
+++ php/configuracion_mails.php
@@ -5,20 +5,15 @@ $SMTPSecure = 'tls';
 // $SMTPSecure = 'SSL';
 // $Port       = 587;
 $Port       = 587;
-$SMTPDebug  =0;
+$SMTPDebug  = 0;
 $SMTPAuth   = true;
 $Username   = 'practifp@outlook.com';
-<<<<<<< HEAD
-$Password   = '(***your password***)';
-=======
-$Password   = '*** YOUR PASSWORD ***';
->>>>>>> 43cc411d3c15b7d9b9336ccb0a2bcc6ec1e355a9
+$Password   = 'Practicas*2022';
 $SetFromMail    = $Username;
 $SetFromNombre = "WEB PractiFP";
 $AddReplyToMail = 'no-reply@mycomp.com';
 $AddReplyToNombre = 'no-reply';
 $Subject    = '*CONTACTO DESDE PRACTIFP*';
-
 $AddAddressMail = $Username;
 $AddAddressNombre = 'Guillermo García';
 
